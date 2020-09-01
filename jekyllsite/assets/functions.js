dd2a34ed23917f40ea344485be469415f523a964
flaslam <flaslam@gmail.com>
2020-09-01 19:31:38 +0100

    jekyll test

---
+++ jekyllsite/assets/functions.js
@@ -0,0 +1,91 @@
+/*
+	When the bandcamp link is pressed, stop all propagation so AmplitudeJS doesn't
+	play the song.
+*/
+let bandcampLinks = document.getElementsByClassName('bandcamp-link');
+
+for( var i = 0; i < bandcampLinks.length; i++ ){
+	bandcampLinks[i].addEventListener('click', function(e){
+		e.stopPropagation();
+	});
+}
+
+
+let songElements = document.getElementsByClassName('song');
+
+for( var i = 0; i < songElements.length; i++ ){
+	/*
+		Ensure that on mouseover, CSS styles don't get messed up for active songs.
+	*/
+	songElements[i].addEventListener('mouseover', function(){
+		this.style.backgroundColor = '#00A0FF';
+
+		this.querySelectorAll('.song-meta-data .song-title')[0].style.color = '#FFFFFF';
+		this.querySelectorAll('.song-meta-data .song-artist')[0].style.color = '#FFFFFF';
+
+		if( !this.classList.contains('amplitude-active-song-container') ){
+			this.querySelectorAll('.play-button-container')[0].style.display = 'block';
+		}
+
+		this.querySelectorAll('img.bandcamp-grey')[0].style.display = 'none';
+		this.querySelectorAll('img.bandcamp-white')[0].style.display = 'block';
+		this.querySelectorAll('.song-duration')[0].style.color = '#FFFFFF';
+	});
+
+	/*
+		Ensure that on mouseout, CSS styles don't get messed up for active songs.
+	*/
+	songElements[i].addEventListener('mouseout', function(){
+		this.style.backgroundColor = '#FFFFFF';
+		this.querySelectorAll('.song-meta-data .song-title')[0].style.color = '#272726';
+		this.querySelectorAll('.song-meta-data .song-artist')[0].style.color = '#607D8B';
+		this.querySelectorAll('.play-button-container')[0].style.display = 'none';
+		this.querySelectorAll('img.bandcamp-grey')[0].style.display = 'block';
+		this.querySelectorAll('img.bandcamp-white')[0].style.display = 'none';
+		this.querySelectorAll('.song-duration')[0].style.color = '#607D8B';
+	});
+
+	/*
+		Show and hide the play button container on the song when the song is clicked.
+	*/
+	songElements[i].addEventListener('click', function(){
+		this.querySelectorAll('.play-button-container')[0].style.display = 'none';
+	});
+}
+
+/*
+	Initializes AmplitudeJS
+*/
+Amplitude.init({
+	continue_next: false,
+	"songs": [
+		{
+			"name": "Commercial Reel",
+			"artist": "Mylo Reid",
+			"album": "Audio Reels",
+			"url": "./audio/commercial.mp3",
+			"cover_art_url": "./img/avatar1.png"
+		},
+		{
+			"name": "Video Game Reel",
+			"artist": "Mylo Reid",
+			"album": "Audio Reels",
+			"url": "./audio/game.mp3",
+			"cover_art_url": "./img/avatar2.jpg"
+		},
+		{
+			"name": "Fighting Game Reel",
+			"artist": "Mylo Reid",
+			"album": "Audio Reels",
+			"url": "./audio/cartoon.mp3",
+			"cover_art_url": "./img/avatar3.png"
+		},
+		{
+			"name": "Additional Commercial Reel",
+			"artist": "Mylo Reid",
+			"album": "Audio Reels",
+			"url": "./audio/explainer.mp3",
+			"cover_art_url": "./img/avatar2.jpg"
+		}
+	]
+});
