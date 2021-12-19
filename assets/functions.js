4b09c416ffee80f2880d3b0bceb46cb6ce7dd53e
flaslam <fahdlaslam@gmail.com>
2021-12-19 20:11:55 +0000

    updated animation reel

--- assets/functions.js
+++ assets/functions.js
@@ -67,17 +67,17 @@ Amplitude.init({
 			"cover_art_url": "./img/avatar1.png"
 		},
 		{
-			"name": "Commercial Reel",
-			"artist": "Mylo Reid",
-			"album": "Audio Reels",
-			"url": "./audio/Mylo_Reid_Commercial_Reel.mp3",
-			"cover_art_url": "./img/avatar2.jpg"
-		},
-		{
 			"name": "Animation Reel",
 			"artist": "Mylo Reid",
 			"album": "Audio Reels",
 			"url": "./audio/Mylo_Reid_Animation_Reel.mp3",
+			"cover_art_url": "./img/avatar2.jpg"
+		},
+		{
+			"name": "Commercial Reel",
+			"artist": "Mylo Reid",
+			"album": "Audio Reels",
+			"url": "./audio/Mylo_Reid_Commercial_Reel.mp3",
 			"cover_art_url": "./img/avatar1.png"
 		}
 		
